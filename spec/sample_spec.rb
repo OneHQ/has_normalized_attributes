de6c4ec752cab689517cd7630a6dc13783cdcc83
mauro torres <maurotorres@gmail.com>
2010-06-17 17:52:43 -0300

    chaging name of constants

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -5,95 +5,95 @@ describe Sample do
     @sample = Sample.new
   end
 
-  describe "ZipCode" do
-    it{@sample.value = "11111";@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
-    it{@sample.value = " 11 1-11 ";@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
-    it{@sample.value = " 111-11 (";@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
-    it{@sample.value = "11,1-11";@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
-    it{@sample.value = "11.111";@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
+  describe "ZIPCODE" do
+    it{@sample.value = "11111";@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
+    it{@sample.value = " 11 1-11 ";@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
+    it{@sample.value = " 111-11 (";@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
+    it{@sample.value = "11,1-11";@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
+    it{@sample.value = "11.111";@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
 
-    it{@sample.value = "111111111";@sample.value.normalize(Normalizations::ZipCode).should == "111111111"}
-    it{@sample.value = "(11111) 1111";@sample.value.normalize(Normalizations::ZipCode).should == "111111111"}
-    it{@sample.value = "11111) -1111";@sample.value.normalize(Normalizations::ZipCode).should == "111111111"}
-    it{@sample.value = "11111.1111";@sample.value.normalize(Normalizations::ZipCode).should == "111111111"}
-    it{@sample.value = "11111 --1111";@sample.value.normalize(Normalizations::ZipCode).should == "111111111"}
-    it{@sample.value = " 11111,1111 ";@sample.value.normalize(Normalizations::ZipCode).should == "111111111"}
+    it{@sample.value = "111111111";@sample.value.normalize(Normalizations::ZIPCODE).should == "111111111"}
+    it{@sample.value = "(11111) 1111";@sample.value.normalize(Normalizations::ZIPCODE).should == "111111111"}
+    it{@sample.value = "11111) -1111";@sample.value.normalize(Normalizations::ZIPCODE).should == "111111111"}
+    it{@sample.value = "11111.1111";@sample.value.normalize(Normalizations::ZIPCODE).should == "111111111"}
+    it{@sample.value = "11111 --1111";@sample.value.normalize(Normalizations::ZIPCODE).should == "111111111"}
+    it{@sample.value = " 11111,1111 ";@sample.value.normalize(Normalizations::ZIPCODE).should == "111111111"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::ZipCode).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::ZipCode).should == nil}
-    it{@sample.value = "(111)";@sample.value.normalize(Normalizations::ZipCode).should == "111"}
-    it{@sample.value = "11111";@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
-    it{@sample.value = 111.11;@sample.value.normalize(Normalizations::ZipCode).should == "11111"}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::ZIPCODE).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::ZIPCODE).should == nil}
+    it{@sample.value = "(111)";@sample.value.normalize(Normalizations::ZIPCODE).should == "111"}
+    it{@sample.value = "11111";@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
+    it{@sample.value = 111.11;@sample.value.normalize(Normalizations::ZIPCODE).should == "11111"}
   end
 
-  describe "Phone" do
-    it{@sample.value = "1111111111";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
-    it{@sample.value = "111 111 1111";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
-    it{@sample.value = " 111 111 1111 ";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
-    it{@sample.value = "111.111.1111";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
-    it{@sample.value = "(111)111-1111";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
-    it{@sample.value = "(111)1111111";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
-    it{@sample.value = " 111-111.1111 ";@sample.value.normalize(Normalizations::Phone).should == "1111111111"}
+  describe "PHONE" do
+    it{@sample.value = "1111111111";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
+    it{@sample.value = "111 111 1111";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
+    it{@sample.value = " 111 111 1111 ";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
+    it{@sample.value = "111.111.1111";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
+    it{@sample.value = "(111)111-1111";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
+    it{@sample.value = "(111)1111111";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
+    it{@sample.value = " 111-111.1111 ";@sample.value.normalize(Normalizations::PHONE).should == "1111111111"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::Phone).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::Phone).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::PHONE).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::PHONE).should == nil}
   end
 
-  describe "Ssn" do
-    it{@sample.value = "111 111 1111";@sample.value.normalize(Normalizations::Ssn).should == "1111111111"}
-    it{@sample.value = " 111 111 1111 ";@sample.value.normalize(Normalizations::Ssn).should == "1111111111"}
-    it{@sample.value = "111.111.1111";@sample.value.normalize(Normalizations::Ssn).should == "1111111111"}
-    it{@sample.value = "(111)111-1111";@sample.value.normalize(Normalizations::Ssn).should == "1111111111"}
-    it{@sample.value = "(111)1111111";@sample.value.normalize(Normalizations::Ssn).should == "1111111111"}
-    it{@sample.value = " 111-111.1111 ";@sample.value.normalize(Normalizations::Ssn).should == "1111111111"}
+  describe "SSN" do
+    it{@sample.value = "111 111 1111";@sample.value.normalize(Normalizations::SSN).should == "1111111111"}
+    it{@sample.value = " 111 111 1111 ";@sample.value.normalize(Normalizations::SSN).should == "1111111111"}
+    it{@sample.value = "111.111.1111";@sample.value.normalize(Normalizations::SSN).should == "1111111111"}
+    it{@sample.value = "(111)111-1111";@sample.value.normalize(Normalizations::SSN).should == "1111111111"}
+    it{@sample.value = "(111)1111111";@sample.value.normalize(Normalizations::SSN).should == "1111111111"}
+    it{@sample.value = " 111-111.1111 ";@sample.value.normalize(Normalizations::SSN).should == "1111111111"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::Ssn).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::Ssn).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::SSN).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::SSN).should == nil}
   end
 
-  describe "TaxID" do
-    it{@sample.value = "12-345";@sample.value.normalize(Normalizations::TaxID).should == "12345"}
-    it{@sample.value = "12.345";@sample.value.normalize(Normalizations::TaxID).should == "12345"}
-    it{@sample.value = "1-2345";@sample.value.normalize(Normalizations::TaxID).should == "12345"}
-    it{@sample.value = "1,2345";@sample.value.normalize(Normalizations::TaxID).should == "12345"}
+  describe "TAXID" do
+    it{@sample.value = "12-345";@sample.value.normalize(Normalizations::TAXID).should == "12345"}
+    it{@sample.value = "12.345";@sample.value.normalize(Normalizations::TAXID).should == "12345"}
+    it{@sample.value = "1-2345";@sample.value.normalize(Normalizations::TAXID).should == "12345"}
+    it{@sample.value = "1,2345";@sample.value.normalize(Normalizations::TAXID).should == "12345"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::TaxID).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::TaxID).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::TAXID).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::TAXID).should == nil}
   end
 
-  describe "Dollar" do
-    it{@sample.value = "$111111";@sample.value.normalize(Normalizations::Dollar).should == "111111"}
-    it{@sample.value = "111,111";@sample.value.normalize(Normalizations::Dollar).should == "111111"}
-    it{@sample.value = "111 111 ";@sample.value.normalize(Normalizations::Dollar).should == "111111"}
-    it{@sample.value = "$111, 111 ";@sample.value.normalize(Normalizations::Dollar).should == "111111"}
+  describe "DOLLAR" do
+    it{@sample.value = "$111111";@sample.value.normalize(Normalizations::DOLLAR).should == "111111"}
+    it{@sample.value = "111,111";@sample.value.normalize(Normalizations::DOLLAR).should == "111111"}
+    it{@sample.value = "111 111 ";@sample.value.normalize(Normalizations::DOLLAR).should == "111111"}
+    it{@sample.value = "$111, 111 ";@sample.value.normalize(Normalizations::DOLLAR).should == "111111"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::Dollar).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::Dollar).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::DOLLAR).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::DOLLAR).should == nil}
   end
 
-  describe "Number" do
-    it{@sample.value = "1,23";@sample.value.normalize(Normalizations::Number).should == "123"}
-    it{@sample.value = "1 23 ";@sample.value.normalize(Normalizations::Number).should == "123"}
+  describe "NUMBER" do
+    it{@sample.value = "1,23";@sample.value.normalize(Normalizations::NUMBER).should == "123"}
+    it{@sample.value = "1 23 ";@sample.value.normalize(Normalizations::NUMBER).should == "123"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::Number).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::Number).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::NUMBER).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::NUMBER).should == nil}
   end
 
-  describe "Percent" do
-    it{@sample.value = " 1 1 ";@sample.value.normalize(Normalizations::Percent).should == "11"}
-    it{@sample.value = "11%";@sample.value.normalize(Normalizations::Percent).should == "11"}
-    it{@sample.value = "%11";@sample.value.normalize(Normalizations::Percent).should == "11"}
-    it{@sample.value = "1 1 % ";@sample.value.normalize(Normalizations::Percent).should == "11"}
+  describe "PERCENT" do
+    it{@sample.value = " 1 1 ";@sample.value.normalize(Normalizations::PERCENT).should == "11"}
+    it{@sample.value = "11%";@sample.value.normalize(Normalizations::PERCENT).should == "11"}
+    it{@sample.value = "%11";@sample.value.normalize(Normalizations::PERCENT).should == "11"}
+    it{@sample.value = "1 1 % ";@sample.value.normalize(Normalizations::PERCENT).should == "11"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::Percent).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::Percent).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::PERCENT).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::PERCENT).should == nil}
   end
 
-  describe "Spaces" do
-    it{@sample.value = "5 0";@sample.value.normalize(Normalizations::Spaces).should == "50"}
+  describe "SPACES" do
+    it{@sample.value = "5 0";@sample.value.normalize(Normalizations::SPACES).should == "50"}
 
-    it{@sample.value = "";@sample.value.normalize(Normalizations::Spaces).should == ""}
-    it{@sample.value = nil;@sample.value.normalize(Normalizations::Spaces).should == nil}
+    it{@sample.value = "";@sample.value.normalize(Normalizations::SPACES).should == ""}
+    it{@sample.value = nil;@sample.value.normalize(Normalizations::SPACES).should == nil}
   end
 end
 
